3213b3e8d5ae87357511f6ee03820d88a84c6e42
Ali Ahangar <110068987+AliTech7@users.noreply.github.com>
2024-06-05 13:26:01 -0400

    Add files via upload

---
+++ CustomTabs/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CustomTabs from "./CustomTabs";
+import TabTest from "./tabTest";
+
+function App() {
+  return (
+    <div className="App">
+      <TabTest />
+    </div>
+  );
+}
+
+export default App;
